235708be5c93cd1358a9427b3a07706f5f68a437
sand1no1 <a00838292@tec.mx>
2025-04-07 06:59:02 -0600

    feat: definición de método que regresa los datos de los estudiantes

--- src/db/student.ts
+++ src/db/student.ts
@@ -1,5 +1,23 @@
+import { StudentDTO } from '../types/student';
+import studentsData from './students.json';
+
 export default class StudentService {
-    async getStudents(){
-        return [1,2,3,4,5,6,7,8,9,10]
-    }
+  private students: StudentDTO[];
+
+  constructor() {
+    this.students = studentsData;
+  }
+
+  getStudents() {
+    return this.students;
+  }
+
+  getStudentsStatus() {
+    return this.students.map(({ matricula, nombre, calificacion, adeudos }) => ({
+      matricula,
+      nombre,
+      calificacion,
+      adeudos
+    }));
+  }
 }
