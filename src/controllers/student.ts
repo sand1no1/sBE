f5406885a62266e3473037b55eb9f2e7f2531d29
sand1no1 <a00838292@tec.mx>
2025-04-07 06:55:53 -0600

    feat: se agrega función para manejar mensaje hacia alumno por su calificación y adeudos

--- src/controllers/student.ts
+++ src/controllers/student.ts
@@ -1,13 +1,39 @@
-import StudentService from "../db/student";
+import StudentService from '../db/student';
 
 export default class StudentController {
-    private service: StudentService;
+  private service: StudentService;
 
-    constructor(service: StudentService){
-        this.service = service;
-    }
+  constructor(service: StudentService) {
+    this.service = service;
+  }
 
-    async getAll(){
-        return this.service.getStudents();
-    }
+  async getAll() {
+    return this.service.getStudents();
+  }
+
+  async getStudensData() {
+    return this.service.getStudentsStatus();
+  }
+
+  async getStudentsMessage() {
+    const students = await this.service.getStudents();
+
+    return students.map(({ matricula, calificacion, adeudos }) => {
+      let mensaje = '';
+
+      if (calificacion > 90 && !adeudos) {
+        mensaje = 'Te graduaste con honores';
+      } else if (calificacion > 90 && adeudos) {
+        mensaje = 'Tenías honores pero tienes un adeudo';
+      } else if (calificacion >= 70 && calificacion <= 90) {
+        mensaje = 'Pasaste muy apenas';
+      } else if (calificacion < 70 && !adeudos) {
+        mensaje = 'Como no tienes adeudo, tienes derecho a un examen de recuperación';
+      } else if (calificacion < 70 && adeudos) {
+        mensaje = 'Eres expulsado';
+      }
+
+      return { matricula, mensaje };
+    });
+  }
 }
